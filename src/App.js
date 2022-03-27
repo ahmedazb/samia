ec97db2d583c67186e29c0c1a82494cd53d79fdf
ahmedazb <ahmedazb9900@gmail.com>
2022-03-27 15:55:16 +0200

    hello

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstComp from './components/FirstComp';
+
+function App() {
+  return (
+    <div>
+    <div>hello</div>
+    <hr/>
+    <FirstComp/>git add README.md
+    </div>
+
+  );
+}
+
+export default App;
